0aeacc0d994f4cc1025919f67976a982bdbb33fc
Alicia Rannou <alicia.rannou23@gmail.com>
2019-03-27 22:59:13 +0100

    Rework of creations in a php array

--- index.php
+++ index.php
@@ -122,230 +122,239 @@
 				<h1>Créations</h1>
 			</div>
 			<div class="creations-container">
-			<div class="slide">
-				<img src="images/Logos.jpg" alt="image01">
+
+			<?php
+
+			$creations = array(
+				[
+					"titre"=>"Pictogrammes",
+					"texte"=>"Création d'icônes pour un logiciel de dessin fictif. Chaque icône représente une fonction. Imagination d'un univers graphique marqué par la monochromie. Vue isométrique stylisée.",
+					"source"=> "images/Logos.jpg",
+					"portrait" => false,
+					"type" => "image",
+					"visible" => true
+				],[
+					"titre"=>"Storyboard photo",
+					"texte"=>"Montage réalisé avec Photoshop. Scénario en trois temps à visée humoristique.",
+					"source"=> "images/L'univers-des-chaussures.jpg",
+					"portrait" => true,
+					"type" => "image",
+					"visible" => false
+				],[
+					"titre"=>"Affiche sur un problème de société",
+					"texte"=>"Affiche contre la pollution et pour le tri. Photos prises en studio, montage avec Photoshop.",
+					"source"=> "images/pbste.jpg",
+					"portrait" => false,
+					"type" => "image",
+					"visible" => true
+				],[
+					"titre"=>"Montage à la Magritte",
+					"texte"=>"Hybridation de deux images issues de contextes très différents pour créer une situation fantastique. Utilisation de Photoshop.",
+					"source"=> "images/baleine_airlines-2.0.jpg",
+					"portrait" => false,
+					"type" => "image",
+					"visible" => false
+				],[
+					"titre"=>"Couverture de livre",
+					"texte"=>"Proposition de couverture pour l'ouvrage de P. Cottier et F. Burban sur le numérique d'aujourd'hui.",
+					"source"=> "images/couverture_v2_01.jpg",
+					"portrait" => false,
+					"type" => "image",
+					"visible" => false
+				],[
+					"titre"=>"Feed Me",
+					"texte"=>"Réalisation d'une charte graphique pour un site conceptuel de recettes de cuisine pour étudiant. Couleurs vives et ambiance jeune.",
+					"source"=> "images/fiche-recette-V1.0.jpg",
+					"portrait" => false,
+					"type" => "image",
+					"visible" => false
+				],[
+					"titre"=>"Affiche pour la formation DUT MMI",
+					"texte"=>"Proposition de visuel présentant la formation DUT MMI. Exemplaire affiché sur les forums étudiants.",
+					"source"=> "images/Affiche-mmi.jpg",
+					"portrait" => false,
+					"type" => "image",
+					"visible" => true
+				],[
+					"titre"=>"Webimmi",
+					"texte"=>"Réalisation de l'une mascotte symbolisant la formation Métiers du Multimédia et de l'Internet. Production en 3D avec Blender",
+					"source"=> "images/mascotte-3D-finale.jpg",
+					"portrait" => false,
+					"type" => "image",
+					"visible" => false
+				],[
+					"titre"=>"Sides",
+					"texte"=>"Création d'une police de caractères, inspirée de motifs tribaux et de labyrinthes. Disponible sur <a href='http://www.dafont.com/fr/sides.font'>Dafont.com</a>",
+					"source"=> "images/Sides.jpg",
+					"portrait" => false,
+					"type" => "image",
+					"visible" => true
+				],[
+					"titre"=>"Labyzor",
+					"texte"=>"Projet tutoré de DUT en 2015. Réalisation en groupe d'un site web intégrant une gestion de comptes utilisateurs, un jeu en javascript, un chat en PHP et des flux musicaux issus de l'API Deezer.",
+					"source"=> "images/labyzor.jpg",
+					"portrait" => false,
+					"type" => "image",
+					"visible" => true
+				],[
+					"titre"=>"Boccard benchmarking",
+					"texte"=>"Site intranet pour la gestion des données relatives aux projets de l'entreprise. Utilisation de HTML / CSS, jQuery et PHP. Fonction de recherche, tri, import et export excel, administration.",
+					"source"=> "images/boccard.jpg",
+					"portrait" => false,
+					"type" => "image",
+					"visible" => true
+				],[
+					"titre"=>"Site web pour artisan",
+					"texte"=>"Proposition de site web pour un artisan local. Codé en HTML et CSS.",
+					"source"=> "images/accueil_2.jpg",
+					"portrait" => false,
+					"type" => "image",
+					"visible" => true
+				],[
+					"titre"=>"Publicité contre un problème de société",
+					"texte"=>"Court clip publicitaire luttant contre le gaspillage du papier, dénonçant la déforestation. Montage avec Premiere Pro.",
+					"source"=> "https://www.youtube.com/embed/8Q8GK6YEg_8",
+					"portrait" => false,
+					"type" => "video",
+					"visible" => true
+				],[
+					"titre"=>"Storyboard",
+					"texte"=>"Storyboard de la publicité contre le gaspillage.",
+					"source"=> "images/Scan%20storyboard_01.jpg",
+					"portrait" => true,
+					"type" => "image",
+					"visible" => true
+				],[
+					"titre"=>"Touttim - J'oublie",
+					"texte"=>"Clip musical tourné pour le groupe Touttim. Un homme évolue dans un appartement couvert de post-it. On suit sa pensée au fil des post-it. Au final, les post-it forment un visage, celui de la femme qu’il aime toujours.",
+					"source"=> "https://www.youtube.com/embed/L3R3iV24RRs",
+					"portrait" => false,
+					"type" => "video",
+					"visible" => true
+				],[
+					"titre"=>"Les circuits courts de consommation",
+					"texte"=>"Micro-trottoir sur le marché de Laval. Les commerçants et clients savent-ils vraiment ce que sont les circuits courts ?",
+					"source"=> "https://www.youtube.com/embed/y57aMMfH_8M",
+					"portrait" => false,
+					"type" => "video",
+					"visible" => true
+				],[
+					"titre"=>"Jeu d'othello en Java",
+					"texte"=>"",
+					"source"=> "images/othello.jpg",
+					"portrait" => false,
+					"type" => "image",
+					"visible" => false
+				],[
+					"titre"=>"Jeu de mastermind",
+					"texte"=>"Jeu réalisé en Python. Choix de la difficulté au lancement de la partie. Projet de fin d’année en Terminale Scientifique option Informatique et Sciences du Numérique.",
+					"source"=> "images/mastermind.jpg",
+					"portrait" => true,
+					"type" => "image",
+					"visible" => true
+				],[
+					"titre"=>"Statisommeil",
+					"texte"=>"Site web présentant de façon ludique des statistiques sur le sommeil, dans le monde et notre quotidien. Réalisé en jQuery. A découvrir <a href='http://perso.univ-lemans.fr/~i140994/'>ici</a>.",
+					"source"=> "images/statisommeil.jpg",
+					"portrait" => false,
+					"type" => "image",
+					"visible" => true
+				],[
+					"titre"=>"Catch me if you Can - Animatique",
+					"texte"=>"Animatique d'un clip humoristique avec des cubes personnifiés représentant les quatres DUT de l'IUT de Laval",
+					"source"=> "https://www.youtube.com/embed/VThFZLVns1I",
+					"portrait" => false,
+					"type" => "video",
+					"visible" => true
+				],[
+					"titre"=>"Catch me if you Can - Modélisation",
+					"texte"=>"Modélisation 3D sous Blender.",
+					"source"=> "images/rendu%202.png",
+					"portrait" => false,
+					"type" => "image",
+					"visible" => true
+				],[
+					"titre"=>"Catch me if you Can - Version Finale",
+					"texte"=>"Animation finale sous Blender",
+					"source"=> "https://www.youtube.com/embed/Th_kFzZ7AYw",
+					"portrait" => false,
+					"type" => "video",
+					"visible" => true
+				],[
+					"titre"=>"Site extranet pour Assura",
+					"texte"=>"Ajout de fonctionnalités en PHP pour la gestion des contrats pour les assurances de transport de marchandises, correction de bugs.",
+					"source"=> "images/assura.jpg",
+					"portrait" => false,
+					"type" => "image",
+					"visible" => true
+				],[
+					"titre"=>"Montage sonore",
+					"texte"=>"Mise en son d'une page de la bande dessinée John Difoul",
+					"source"=> "images/johnDifoul.mp4",
+					"portrait" => false,
+					"type" => "video",
+					"visible" => false
+				],[
+					"titre"=>"Cuisinaute",
+					"texte"=>"Animation After Effects présentant une application fictive de recettes de cuisine pour tablette, à destination des enfants.",
+					"source"=> "https://www.youtube.com/embed/Bj-sgFxA5tU",
+					"portrait" => false,
+					"type" => "video",
+					"visible" => true
+				],[
+					"titre"=>"Greed's Gale",
+					"texte"=>"Projet tutoré DUT 2016. Jeu vidéo en 2D mélant un shoot’em up et des phases d’infiltration dans un univers médiéval et fantastique. Vous incarnez Robb, un voleur qui parcourt le pays à dos de dragon pour subtiliser les joyaux du Roi. Sur son chemin se dresseront de nombreux ennemis. Réalisé en groupe avec Unity.",
+					"source"=> "images/gg.jpg",
+					"portrait" => false,
+					"type" => "image",
+					"visible" => true
+				],[
+					"titre"=>"Mad Jack",
+					"texte"=>"Utilisation du logiciel Max pour appilquer des effets vidéo et des incrustations sur un tournage en temps réel, en synchronie avec une musique.",
+					"source"=> "https://www.youtube.com/embed/1k-B0s3KspM",
+					"portrait" => false,
+					"type" => "video",
+					"visible" => true
+				],[
+					"titre"=>"Institut d'Informatique Claude Chappe",
+					"texte"=>"Proposition de logo. Dessin vectoriel avec Illustrator.",
+					"source"=> "images/logo-ic2-v3.0.png",
+					"portrait" => false,
+					"type" => "image",
+					"visible" => false
+				],[
+					"titre"=>"xxxxx",
+					"texte"=>"xxxxx",
+					"source"=> "images/xxxx.jpg",
+					"portrait" => false,
+					"type" => "image",
+					"visible" => true
+				]
+				);
+
+foreach($creations as $crea) {
+	if ($crea["visible"]==true) {
+		$class = $crea["portrait"]==true ? "slide portrait": "slide"; 
+		echo '<div class="'.$class.'">';
+
+		if ($crea["type"]=="image") {
+			echo '<img src="'.$crea["source"].'" alt="'.$crea["titre"].'">';
+		} else if ($crea["type"]=="video") {
+			echo '<iframe width="560" height="315" src="'.$crea["source"].'" frameborder="0" allowfullscreen></iframe>';
+		}
+	?>
 				<div>
-					<p>Pictogrammes</p>
-					<span>Création d'icônes pour un logiciel de dessin fictif. Chaque icône représente une fonction. Imagination d'un univers graphique marqué par la monochromie. Vue isométrique stylisée.</span>
+					<p><?php echo($crea["titre"]); ?></p>
+					<span><?php echo($crea["texte"]); ?></span>
 				</div>
 			</div>
 
-			<div class="slide">
-				<img src="images/L'univers-des-chaussures.jpg" alt="image02">
-				<div>
-					<p>Storyboard photo</p>
-					<span>Montage réalisé avec Photoshop. Scénario en trois temps à visée humoristique.</span>
-				</div>
-			</div>
+	<?php 
+	}
+}
+	?>
 
-			<div class="slide">
-				<img src="images/pbste.jpg" alt="image03">
-				<div>
-					<p>Affiche sur un problème de société</p>
-					<span>Affiche contre la pollution et pour le tri. Photos prises en studio, montage avec Photoshop.</span>
-				</div>
-			</div>
 
-			<div class="slide">
-				<img src="images/baleine_airlines-2.0.jpg" alt="image04">
-				<div>
-					<p>Montage à la Magritte</p>
-					<span>Hybridation de deux images issues de contextes très différents pour créer une situation fantastique. Utilisation de Photoshop.</span>
-				</div>
-			</div>
-
-			<div class="slide">
-				<img src="images/couverture_v2_01.jpg" alt="image05">
-				<div>
-					<p>Couverture de livre</p>
-					<span>Proposition de couverture pour l'ouvrage de P. Cottier et F. Burban sur le numérique d'aujourd'hui.</span>
-				</div>
-			</div>
-
-			<div class="slide">
-				<img src="images/fiche-recette-V1.0.png" alt="image05">
-				<div>
-					<p>Feed Me</p>
-					<span>Réalisation d'une charte graphique pour un site conceptuel de recettes de cuisine pour étudiant. Couleurs vives et ambiance jeune.</span>
-				</div>
-			</div>
-
-			<div class="slide">
-				<img src="images/Affiche-mmi.jpg" alt="image05">
-				<div>
-					<p>Affiche pour la formation DUT MMI</p>
-					<span>Proposition de visuel présentant la formation DUT MMI</span>
-				</div>
-			</div>
-			<div class="slide">
-				<img src="images/mascotte-3D-finale.jpg" alt="image05">
-				<div>
-					<p>Webimmi</p>
-					<span>Réalisation de l'une mascotte symbolisant la formation Métiers du Multimédia et de l'Internet. Production en 3D avec Blender</span>
-				</div>
-			</div>
-
-			<div class="slide">
-				<img src="images/Sides.jpg" alt="image05">
-				<div>
-					<p>Sides</p>
-					<span>Création d'une police de caractères, inspirée de motifs tribaux et de labyrinthes. Disponible sur <a href="http://www.dafont.com/fr/sides.font">Dafont.com</a></span>
-				</div>
-			</div>
-
-			<div class="slide">
-				<img src="images/labyzor.jpg" alt="image01">
-				<div>
-					<p>Labyzor</p>
-					<span>Projet tutoré 2015. Réalisation en groupe d'un site web intégrant une gestion de comptes utilisateurs, un jeu en javascript, un chat en Php et des flux musicaus issus de Deezer.</span>
-				</div>
-			</div>
-
-			<div class="slide">
-				<img src="images/boccard.jpg" alt="image02">
-				<div>
-					<p>Boccard benchmarcking</p>
-					<span>Site intranet pour la gestion des données relatives aux projets de l'entreprise. Utilisation de HTML / CSS, jQuery et PHP. Fonction de recherche, tri, import et export excel, administration.</span>
-				</div>
-			</div>
-
-			<div class="slide">
-				<img src="images/accueil_2.jpg" alt="image02">
-				<div>
-					<p>Site web pour artisan</p>
-					<span>Proposition de site web pour un artisan local. Codé en HTML et CSS.</span>
-				</div>
-			</div>
-
-			<div class="slide">
-			<iframe width="560" height="315" src="https://www.youtube.com/embed/8Q8GK6YEg_8" frameborder="0" allowfullscreen></iframe>
-				<div>
-					<p>Publicité contre un problème de société</p>
-					<span>Court clip publicitaire luttant contre le gaspillage du papier, dénonçant la déforestation. Montage avec Premiere Pro.</span>
-				</div>
-			</div>
-
-			<div class="slide">
-				<img src="images/Scan%20storyboard_01.jpg" alt="" />
-				<div>
-					<p>Storyboard<p>
-					<span>Storyboard de la publicité contre le gaspillage.</span>
-				</div>
-			</div>
-
-			<div class="slide">
-				<iframe width="560" height="315" src="https://www.youtube.com/embed/L3R3iV24RRs" frameborder="0" allowfullscreen></iframe>
-				<div>
-					<p>Touttim - "J'oublie"</p>
-					<span>Clip musical tourné pour le groupe Touttim. Un homme évolue dans un appartement couvert de post-it. On suit sa pensée au fil des post-it. Au final, les post-it forment un visage, celui de la femme qu’il aime toujours.</span>
-				</div>
-			</div>
-
-			<div class="slide">
-				<iframe width="560" height="315" src="https://www.youtube.com/embed/y57aMMfH_8M" frameborder="0" allowfullscreen></iframe>
-				<div>
-					<p>Les circuits courts de consommation</p>
-					<span>Micro-trottoir sur le marché de Laval. Les commerçants et clients savent-ils vraiment ce que sont les circuits courts ?</span>
-				</div>
-			</div>
-
-<!--
-			<div class="slide">
-				<img src="images/othello.jpg" alt="image02">
-				<div>
-					<p>Jeu d'othello en Java</p>
-				</div>
-			</div>
--->
-
-			<div class="slide">
-				<img src="images/mastermind.jpg" alt="image02">
-				<div>
-					<p>Jeu de mastermind</p>
-					<span>Jeu réalisé en Python. Choix de la difficulté au lancement de la partie. Projet de fin d’année en Terminale Scientifique option Informatique et Sciences du Numérique.</span>
-				</div>
-			</div>
-
-			<div class="slide">
-				<img src="images/statisommeil.jpg" alt="image02">
-				<div>
-					<p>Statisommeil</p>
-					<span>Site web présentant de façon ludique des statistiques sur le sommeil, dans le monde et notre quotidien. Réalisé en jQuery. A découvrir <a href="http://perso.univ-lemans.fr/~i140994/">ici</a>.</span>
-				</div>
-			</div>
-
-			<div class="slide">
-				<iframe width="560" height="315" src="https://www.youtube.com/embed/VThFZLVns1I" frameborder="0" allowfullscreen></iframe>
-				<div>
-					<p>Catch me if you Can - Animatique</p>
-					<span>Animatique d'un clip humoristique avec des cubes personnifiés représentant les quatres DUT de l'IUT de Laval</span>
-				</div>
-			</div>
-
-			<div class="slide">
-				<img src="images/rendu%202.png" alt="image02">
-				<div>
-					<p>Catch me if you Can - Modelisation</p>
-					<span>Modélisation 3D sous Blender.</span>
-				</div>
-			</div>
-
-			<div class="slide">
-				<iframe width="560" height="315" src="https://www.youtube.com/embed/Th_kFzZ7AYw" frameborder="0" allowfullscreen></iframe>
-				<div>
-					<p>Catch me if you Can - Version Finale</p>
-					<span>Animation finale sous Blender</span>
-				</div>
-			</div>
-
-			<div class="slide">
-				<img src="images/assura.jpg" alt="image02">
-				<div>
-					<p>Site extranet pour Assura</p>
-					<span>Ajout de fonctionnalités en PHP pour la gestion des contrats pour les assurances de transport de marchandises, correction de bugs</span>
-				</div>
-			</div>
-
-<!--
-			<div class="slide" id="slide24" data-anchor="slide1">
-				<video src="images/johnDifoul.mp4" width="480px" controls>Oups</video>
-				<div>
-					<p>Montage sonore sur la bande dessinée John difoul</p>
-				</div>
-			</div>
--->
-
-			<div class="slide">
-				<iframe width="560" height="315" src="https://www.youtube.com/embed/Bj-sgFxA5tU" frameborder="0" allowfullscreen></iframe>
-				<div>
-					<p>Cuisinaute</p>
-					<span>Animation After Effects présentant une application fictive de recettes de cuisine pour tablette, à destination des enfants.</span>
-				</div>
-			</div>
-
-			<div class="slide">
-				<img src="images/gg.jpg" alt="image02">
-				<div>
-					<p>Greed's Gale</p>
-					<span>Projet tutoré 2016. Jeu vidéo en 2D mélant un shoot’em up et des phases d’infiltration dans un univers médiéval et fantastique. Vous incarnez Robb, un voleur qui parcours le pays à dos de dragon pour subtiliser les joyaux du Roi. Sur son chemin se dresseront de nombreux ennemis. Réalisé en groupe avec Unity.</span>
-				</div>
-			</div>
-
-			<div class="slide">
-				<iframe width="560" height="315" src="https://www.youtube.com/embed/1k-B0s3KspM" frameborder="0" allowfullscreen></iframe>
-				<div>
-					<p>Mad Jack</p>
-					<span>Utilisation de Max pour appilquer des effets vidéo et des incrustations sur un tournage en temps réel, en synchronie avec une musique.</span>
-				</div>
-			</div>
-			
-			<div class="slide">
-				<img src="images/logo-ic2-v3.0.png" alt="image02">
-				<div>
-					<p>Institut d'Informatique Claude Chappe</p>
-					<span>Proposition de logo. Dessin vectoriel avec Illustrator.</span>
-				</div>
-			</div>
 			<div class="slide">
 				<article id="gifs">
 					<img src="images/main%20version%203.gif" alt="image02">
